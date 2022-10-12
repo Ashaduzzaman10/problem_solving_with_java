d6026a8680aa7fc2ab6225b09d0914e9c7340fca
Ashaduzzaman10 <infoaj549814@gmail.com>
2022-10-12 21:21:32 +0600

    first repo done

--- beginner_level/hello.java
+++ beginner_level/hello.java
@@ -1,8 +1,11 @@
+package beginner_level;
+
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("I am Ashaduzzaman Akash");
         System.out.println("this is my problem solving repo ");
+        System.out.println("tnq for showing my repo if you have a better solution then me then feel free to know me again thank you so much for visiting my repo😍");
     }
 
 }
